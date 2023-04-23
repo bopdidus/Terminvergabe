c9629d365eca83e9fda928ba09989b9b47c7b0e2
bopdidus <bopdidus@gmail.com>
2023-04-24 01:47:01 +0200

    Login Form validation added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators, FormGroupDirective, NgForm, FormControl, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent  {
 hide = true;
+
+ constructor( ) {}
+
+ loginForm = new FormGroup({
+      emailCtrl: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$") ]),
+      passwordCtrl: new FormControl ('', [Validators.required, Validators.minLength(8)])
+    })
+
+
 }
