5b12e6c4c28d95ad93a516fb3c1763b1b19fa252
bopdidus <bopdidus@gmail.com>
2023-05-01 21:15:16 +0200

    First merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,33 @@
+//-------------------MODULES(START)----------------------
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader'; 
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClientModule, HttpClient} from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { HomeScreenComponent } from './home-screen/home-screen.component';
-import { AppointmentListComponent } from './appointment-list/appointment-list.component';
-
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//-------------------MODULES(END)----------------------
+
+//-------------------COMPONENTS(START)----------------------
+import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { AppointmentListComponent } from './appointment-list/appointment-list.component';
 import { AppointmentFormComponent } from './appointment-form/appointment-form.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+//-------------------COMPONENTS(END)----------------------
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -52,27 +49,22 @@ export function createTranslateLoader(http: HttpClient) {
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
     MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatButtonModule,
     MatToolbarModule,
     MatStepperModule,
     MatTableModule,
     AppRoutingModule,
      BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    MatStepperModule,
-    MatInputModule,
     MatIconModule,
     ReactiveFormsModule,
     MatButtonModule,
     MatCardModule,
     MatDividerModule,
-    MatFormFieldModule,
     HttpClientModule,
     BrowserAnimationsModule,
     TranslateModule.forRoot({
