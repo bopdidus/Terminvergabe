8f98c99fcf5bb24395e3bb2f9e87f36cff846f85
bopdidus <bopdidus@gmail.com>
2023-05-16 23:10:48 +0200

    not found page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-
+    /*{path:'**', component:PageNotFoundComponent },*/
+    { path: '',   redirectTo: '/user/login', pathMatch: 'full' },
   {path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule)},
   {path: 'user', loadChildren: () => import('./user/user.module').then(u => u.UserModule)},
 ];
